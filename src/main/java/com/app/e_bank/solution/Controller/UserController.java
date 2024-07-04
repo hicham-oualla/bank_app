45a381d608bd3c0ba0db2d0e69ba893aec456414
hicham-oualla <hich2022am.oualla@gmail.com>
2024-07-04 11:11:50 +0100

    creation de  user controller

---
+++ src/main/java/com/app/e_bank/solution/Controller/UserController.java
@@ -0,0 +1,33 @@
+package com.app.e_bank.solution.Controller;
+
+import com.app.e_bank.solution.Model.Utilisateur;
+import com.app.e_bank.solution.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<Utilisateur> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Utilisateur> getUserById(@PathVariable int id) {
+        Utilisateur user = userService.getUserById(id);
+        return ResponseEntity.ok(user);
+    }
+
+
+
+
+}
