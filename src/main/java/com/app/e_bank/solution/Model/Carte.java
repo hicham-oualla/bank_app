a98248d53a14efb6b5d7f93c833eb5fa0b01f6e9
hicham-oualla <hich2022am.oualla@gmail.com>
2024-07-05 15:30:27 +0100

    creation carte model& carte repository

---
+++ src/main/java/com/app/e_bank/solution/Model/Carte.java
@@ -0,0 +1,30 @@
+package com.app.e_bank.solution.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Carte {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idCarte;
+    private String numeroCarte;
+    private LocalDate dateExpiration;
+    private String typeCarte;
+    private boolean active;
+
+    @ManyToOne
+    @JoinColumn(name = "compte_id")
+    private Compt compte;
+
+}
+
+
---
+++ src/main/java/com/app/e_bank/solution/Repository/CarteRepository.java
@@ -0,0 +1,8 @@
+package com.app.e_bank.solution.Repository;
+
+import com.app.e_bank.solution.Model.Carte;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CarteRepository extends JpaRepository<Carte, Integer> {
+
+}
